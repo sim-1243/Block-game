ef405102ec42da8714280431814735bac8ea8b57
sim-1243 <simon.wilde@outlook.de>
2024-12-19 20:21:48 +0100

    bug-fix

--- backbone.py
+++ backbone.py
@@ -34,13 +34,13 @@ def richtung(x1,x2):
         return True
 def player_out(px,py,width,height,player_width,player_height):
     if px+player_width>=width:
-        px-=6
+        px-=5
     elif px<=0:
-        px+=6
+        px+=5
     elif py+player_height*1.75>=height:
-        py-=6
+        py-=5
     elif py<=0:
-        py+=6
+        py+=5
     return px,py
 def out_of_screen(px,py,x,y,width,height):
     if x>width:
